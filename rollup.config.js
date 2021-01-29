1204312065fa613a29381df7095a9b7fa553fe83
Kyle Owsen <kyle.owsen@gmail.com>
2021-01-29 15:29:41 -0800

    Initial commit

---
+++ rollup.config.js
@@ -0,0 +1,88 @@
+import svelte from 'rollup-plugin-svelte';
+import commonjs from '@rollup/plugin-commonjs';
+import resolve from '@rollup/plugin-node-resolve';
+import livereload from 'rollup-plugin-livereload';
+import { terser } from 'rollup-plugin-terser';
+import cleaner from 'rollup-plugin-cleaner';
+import { spawn } from 'child_process';
+import scss from 'rollup-plugin-scss';
+
+// eslint-disable-next-line no-undef
+const production = !process.env.ROLLUP_WATCH;
+
+function serve() {
+  let server;
+
+  function toExit() {
+    if (server) server.kill(0);
+  }
+
+  return {
+    writeBundle() {
+      if (server) return;
+      server = spawn('npm', ['run', 'start', '--', '--dev'], {
+        stdio: ['ignore', 'inherit', 'inherit'],
+        shell: true,
+      });
+
+      // eslint-disable-next-line no-undef
+      process.on('SIGTERM', toExit);
+      // eslint-disable-next-line no-undef
+      process.on('exit', toExit);
+    },
+  };
+}
+
+export default {
+  input: 'src/main.js',
+  output: {
+    sourcemap: !production,
+    format: 'iife',
+    name: 'app',
+    file: 'public/build/bundle.js',
+  },
+  plugins: [
+    cleaner({
+      targets: production ? ['./public/build'] : [],
+    }),
+    svelte({
+      compilerOptions: {
+        // enable run-time checks when not in production
+        dev: !production,
+      },
+    }),
+    // we'll extract any component CSS out into
+    // a separate file - better for performance
+    scss({
+      output: 'public/build/bundle.css',
+      outputStyle: 'compressed',
+      watch: 'src/styles',
+    }),
+
+    // If you have external dependencies installed from
+    // npm, you'll most likely need these plugins. In
+    // some cases you'll need additional configuration -
+    // consult the documentation for details:
+    // https://github.com/rollup/plugins/tree/master/packages/commonjs
+    resolve({
+      browser: true,
+      dedupe: ['svelte'],
+    }),
+    commonjs(),
+
+    // In dev mode, call `npm run start` once
+    // the bundle has been generated
+    !production && serve(),
+
+    // Watch the `public` directory and refresh the
+    // browser on changes when not in production
+    !production && livereload('public'),
+
+    // If we're building for production (npm run build
+    // instead of npm run dev), minify
+    production && terser(),
+  ],
+  watch: {
+    clearScreen: false,
+  },
+};

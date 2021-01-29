ae166110bce27bbf25cb7ca2e9b2501c659e2c1a
Kyle Owsen <kyle.owsen@gmail.com>
2021-01-29 15:50:18 -0800

    Clean up styles and make page responsive

--- src/App.svelte
+++ src/App.svelte
@@ -1,35 +1,14 @@
 <script>
-  export let name;
-
-  import './styles/global.scss';
+  import './setup_responsive';
 </script>
 
 <main>
-  <h1>Hello {name}!</h1>
-  <p>
-    Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn
-    how to build Svelte apps.
-  </p>
+  <h1>Kyle's go-bag</h1>
+  <p>Barebones responsive site template in Svelte!</p>
 </main>
 
 <style>
   main {
-    text-align: center;
-    padding: 1em;
-    max-width: 240px;
-    margin: 0 auto;
-  }
-
-  h1 {
-    color: #ff3e00;
-    text-transform: uppercase;
-    font-size: 4em;
-    font-weight: 100;
-  }
-
-  @media (min-width: 640px) {
-    main {
-      max-width: none;
-    }
+    margin: 24px;
   }
 </style>
---
+++ src/setup_responsive.js
@@ -0,0 +1,10 @@
+import './styles/global.scss';
+import './styles/scale.scss';
+import './styles/scroll.scss';
+
+new ResizeObserver(([event]) => {
+  document.documentElement.style.setProperty(
+    '--page-width',
+    event.contentRect.width
+  );
+}).observe(document.documentElement);

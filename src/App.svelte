b3a1d3672da75471ed60e01091f2890721c962f9
Kyle Owsen <kyle.owsen@gmail.com>
2021-01-29 16:18:17 -0800

    Add spinner to template

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,14 @@
 <script>
   import './setup_responsive';
+
+  import Spinner from './util/Spinner.svelte';
 </script>
 
 <main>
   <h1>Kyle's go-bag</h1>
   <p>Barebones responsive site template in Svelte!</p>
+
+  <Spinner />
 </main>
 
 <style>
---
+++ src/util/Spinner.svelte
@@ -0,0 +1,22 @@
+<script>
+  import { onDestroy } from 'svelte';
+
+  export let seed = '...';
+  export let stepLength = 200;
+  export let fontSize = 30;
+
+  $: style = `font-size: ${fontSize}px;`;
+
+  let counter = 0;
+  $: loadStr = seed.slice(0, counter % (seed.length + 1));
+
+  const counterInterval = setInterval(() => {
+    counter += 1;
+  }, stepLength);
+
+  onDestroy(() => {
+    clearInterval(counterInterval);
+  });
+</script>
+
+<span {style}>{loadStr}</span>
